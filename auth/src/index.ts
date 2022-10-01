c1cd85e0a268104f3318439e334bf15f98d4e54c
Oybek Toirov <oybek2891@gmail.com>
2022-10-01 10:53:33 -0400

    Initial commit

---
+++ auth/src/index.ts
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) throw new Error('JWT_KEY must be defined');
+
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('connected to MongoDB');
+  } catch (err) {
+    console.log(err);
+  }
+  
+}
+
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("hi");
+});
+
+app.listen(3000, () => {
+  console.log("listening on 3000!!");
+});
+
+start();
